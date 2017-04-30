04a1fb149390f1c8560042b259b02f73f24c5434
shaishav-mtech <shaishav.shah@marutitech.com>
2017-04-30 12:31:22 +0530

    Single .js file between android and ios.

---
+++ quickping.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import {
+	StyleSheet,
+	Text,
+	View,
+	StatusBar
+} from 'react-native'
+
+import AppHeader from './app/components/AppHeader'
+import OptionsList from './app/components/OptionsList'
+
+import GlobStyles from './app/config/styles'
+import GlobStrings from './app/config/strings'
+
+import SelectOptions from './app/config/options'
+import IndexStyles from './index.styles'
+
+export default class quickping extends Component {
+	render() {
+		return (
+			<View>
+				<StatusBar
+					backgroundColor={GlobStyles.primary_dark5}
+					barStyle="light-content"
+				/>
+				<AppHeader
+					titleText={GlobStrings.appName_upperCamel}
+				></AppHeader>
+				<View style={IndexStyles.optionsListStyle}>
+					<OptionsList
+						selectOptions={SelectOptions}
+					></OptionsList>
+				</View>
+			</View>
+		);
+	}
+}
